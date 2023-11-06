a0dbf1949bbe159f3c54e440e5a4c51b27783848
kevinngonzales <kevinngstem@gmail.com>
2023-11-06 15:31:56 -0800

    added js file

---
+++ script.js
@@ -0,0 +1,20 @@
+let div1 = document.getElementById("example1");
+div1.style.backgroundColor = "yellow";
+
+let par1 = document.getElementById("example2");
+par1.style.fontSize = "24px";
+
+let heading1 = document.querySelector(".example3");
+heading1.style.color =  "red";
+
+let button1 = document.getElementById("example4");
+button1.style.borderStyle = "solid";
+
+let backgroundImg = document.getElementById("example5");
+backgroundImg.style.backgroundImage = "url(https://t3.ftcdn.net/jpg/06/16/73/38/360_F_616733889_AmXsyEL0Xx8foHNB37oxzUZ1jfmVvqll.jpg)";
+
+let par2 = document.getElementById("example6");
+par2.style.display = "none";
+
+let list1 = document.querySelector (".example7")
+list1.style.color = "green";
